3de6010a524dd4391f8f732ed2dbbeb3ada99821
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 15:46:16 +0530

    cm

--- text.c
+++ text.c
@@ -145,3 +145,57 @@ int searchInsert(int* nums, int numsSize, int target) {
     
     return low;
 }
+
+int climbStairs(int n) {
+    int i=0,j=1,k,cnt=0;
+    while(cnt!=n)
+    {k=i+j;
+        i=j;
+        j=k;
+        cnt++; }
+    return k;
+
+    void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int p1=m-1;
+    int p2=n-1;
+    int p=m+n-1;
+    while(p1>=0 && p2>=0){
+        if(nums1[p1]>nums2[p2])
+            nums1[p--]=nums1[p1--];
+        else
+        nums1[p--]=nums2[p2--];
+    }
+    while(p2>=0)
+     nums1[p--]=nums2[p2--];
+}
+
+char* addBinary(char* a, char* b) {
+    int i=strlen(a), j=strlen(b);
+    int aux=0,k=fmax(i, j)+2;
+    char* result = (char*) malloc (sizeof(char) * k);
+    
+    result[--k] = '\0';
+    i--; j--;
+    while(--k >= 0){
+        aux += i >= 0 ? a[i--] - '0': 0;
+        aux += j >= 0 ? b[j--] - '0': 0;
+        result[k] = aux % 2 + '0';
+        aux /= 2;
+    }
+    if(result[0] == '0') return result+1;
+    return result;
+}
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int p1=m-1;
+    int p2=n-1;
+    int p=m+n-1;
+    while(p1>=0 && p2>=0){
+        if(nums1[p1]>nums2[p2])
+            nums1[p--]=nums1[p1--];
+        else
+        nums1[p--]=nums2[p2--];
+    }
+    while(p2>=0)
+     nums1[p--]=nums2[p2--];
+}
