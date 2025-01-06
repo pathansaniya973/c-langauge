5bc21b4a12245308004af211b12e3a7e533ea0ca
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 10:13:59 +0530

    cc

--- text.c
+++ text.c
@@ -125,3 +125,23 @@ int strStr(char* haystack, char* needle) {
 
     return result = (j == needle_size) ? (i - needle_size) : -1;
 }
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid; 
+        }
+        if (nums[mid] < target) {
+            low = mid + 1;  
+        } else {
+            high = mid - 1; 
+        }
+    }
+    
+    return low;
+}
