b34553b769d2b6d55b873bcd51cdb56354b2e342
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 15:48:23 +0530

    hg

--- text.c
+++ text.c
@@ -199,3 +199,23 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     while(p2>=0)
      nums1[p--]=nums2[p2--];
 }
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid; 
+        }
+        if (nums[mid] < target) {
+            low = mid + 1;  
+        } else {
+            high = mid - 1; 
+        }
+    }
+    
+    return low;
+}
