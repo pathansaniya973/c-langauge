4cdda070d56e0708712243f96fc678d6160785ac
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 10:05:55 +0530

    added c language

--- text.c
+++ text.c
@@ -1,1 +1,18 @@
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int n, int target, int* returnSize) {
+    int *result=(int*)malloc(2*sizeof(int));
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+        if(nums[i]+nums[j]==target){
+            result [0]=i;
+            result [1]=j;
+            *returnSize=2;
+            return result;
+        }
+    }
+}
+*returnSize=0;
+return 0;
+}
