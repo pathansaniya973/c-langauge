6de4d17e7bd8ac5629d426910d98d27564c375af
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 10:12:17 +0530

    bb

--- text.c
+++ text.c
@@ -104,3 +104,24 @@ char * longestCommonPrefix(char ** str, int size){
  //   printf("%s",s);
     return s;
 }
+
+int strStr(char* haystack, char* needle) {
+    int haystack_size = strlen(haystack);
+    int needle_size = strlen(needle);
+    int result = -1;
+    int i = 0;  // haystack
+    int j = 0;  // needle
+
+    while (i < (haystack_size) && j < needle_size) {
+        if (haystack[i] == needle[j]) {
+            i++;
+            j++;
+        }
+        else {
+            i = i - j + 1;
+            j = 0;
+        }
+    }
+
+    return result = (j == needle_size) ? (i - needle_size) : -1;
+}
