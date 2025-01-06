c975d28bba96e98d839001aa34098e1e8efcd150
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 15:55:52 +0530

    gf

--- text.c
+++ text.c
@@ -219,3 +219,24 @@ int searchInsert(int* nums, int numsSize, int target) {
     
     return low;
 }
+
+int strStr(char* haystack, char* needle) {
+    int haystack_size = strlen(haystack);
+    int needle_size = strlen(needle);
+    int result = -1;
+    int i = 0;  // haystack
+    int j = 0;  // needle
+
+    while (i < (haystack_size) && j < needle_size) {
+        if (haystack[i] == needle[j]) {
+            i++;
+            j++;
+        }
+        else {
+            i = i - j + 1;
+            j = 0;
+        }
+    }
+
+    return result = (j == needle_size) ? (i - needle_size) : -1;
+}
