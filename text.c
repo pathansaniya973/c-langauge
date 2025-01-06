ccdf15499d2f7c2463c45de3660219547c73033d
Pathansaniya973 <pathansaniya98703@gmail.com>
2025-01-06 16:00:52 +0530

    su

--- text.c
+++ text.c
@@ -251,3 +251,14 @@ int removeDuplicates(int* nums, int numsSize) {
   }
   return c;
 }
+
+int removeDuplicates(int* nums, int numsSize) {
+  int c=1;
+  for(int i=0;i<numsSize;i++){
+    if( nums[i]!=nums[c-1]){
+        nums[c]=nums[i];
+        c++;
+    }
+  }
+  return c;
+}
